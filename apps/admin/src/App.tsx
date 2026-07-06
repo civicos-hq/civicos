d2c32862ae48cb2c9ab49749cf8765ce09f64b2f
Gino <ginowine1@gmail.com>
2026-07-07 00:07:50 +0100

    fix: remove ability for admin to create a rep or org

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -4,12 +4,10 @@ import { OverviewPage } from './routes/OverviewPage';
 import { UsersPage } from './routes/UsersPage';
 import { UserDetailPage } from './routes/UserDetailPage';
 import { OrganizationsPage } from './routes/OrganizationsPage';
-import { OrganizationCreatePage } from './routes/OrganizationCreatePage';
 import { CommunitiesPage } from './routes/CommunitiesPage';
 import { CommunityCreatePage } from './routes/CommunityCreatePage';
 import { CommunityDetailPage } from './routes/CommunityDetailPage';
 import { RepresentativesPage } from './routes/RepresentativesPage';
-import { RepresentativeCreatePage } from './routes/RepresentativeCreatePage';
 import { FlagsPage } from './routes/FlagsPage';
 import { DirectHidePage } from './routes/DirectHidePage';
 import { AuditPage } from './routes/AuditPage';
@@ -29,13 +27,11 @@ export function App() {
           <Route path="/users" element={<UsersPage />} />
           <Route path="/users/:id" element={<UserDetailPage />} />
           <Route path="/organizations" element={<OrganizationsPage />} />
-          <Route path="/organizations/new" element={<OrganizationCreatePage />} />
           <Route path="/organizations/:id" element={<OrganizationDetailPage />} />
           <Route path="/communities" element={<CommunitiesPage />} />
           <Route path="/communities/new" element={<CommunityCreatePage />} />
           <Route path="/communities/:id" element={<CommunityDetailPage />} />
           <Route path="/representatives" element={<RepresentativesPage />} />
-          <Route path="/representatives/new" element={<RepresentativeCreatePage />} />
           <Route path="/applications" element={<ApplicationsPage />} />
           <Route path="/applications/:kind/:id" element={<ApplicationDetailPage />} />
           <Route path="/moderation/direct-hide" element={<DirectHidePage />} />
